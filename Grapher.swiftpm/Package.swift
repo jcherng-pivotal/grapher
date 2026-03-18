aa9455f520380b34491429660a2db62c7e4a060e
jcherng-pivotal <jeff.cherng@a-j.us>
2026-03-18 10:40:29 -0500

    fix issue

--- Grapher.swiftpm/Package.swift
+++ Grapher.swiftpm/Package.swift
@@ -1,35 +1,17 @@
 // swift-tools-version: 5.7
 
 import PackageDescription
-import AppleProductTypes
 
 let package = Package(
     name: "Grapher",
     platforms: [
-        .iOS("16.0"),
-        .macOS("13.0")
+        .iOS(.v16),
+        .macOS(.v13)
     ],
     products: [
-        .iOSApplication(
-            name: "Grapher",
-            targets: ["AppModule"],
-            bundleIdentifier: "com.example.Grapher",
-            teamIdentifier: "",
-            displayVersion: "1.0",
-            bundleVersion: "1",
-            appIcon: .placeholder(icon: .chart),
-            accentColor: .presetColor(.blue),
-            supportedDeviceFamilies: [
-                .pad,
-                .phone,
-                .mac
-            ],
-            supportedInterfaceOrientations: [
-                .portrait,
-                .landscapeRight,
-                .landscapeLeft,
-                .portraitUpsideDown(.when(deviceFamilies: [.pad]))
-            ]
+        .executable(
+            name: "GrapherApp",
+            targets: ["AppModule"]
         )
     ],
     targets: [
